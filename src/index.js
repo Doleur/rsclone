feac28e83b7c5f8c55b7677bd5e40218563835b1
Alena-Zykava <zykava.alena@gmail.com>
2021-01-12 10:22:50 +0300

    init: swiper

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import './assets/scss/style.scss'
+import './assets/scss/swiper.scss'
+import './assets/scss/custom.scss'
+import './assets/scss/tab.scss'
 
 
 
-import './assets/js/test.js'
+
+
+
+// import './assets/js/test.js'
+import './assets/js/swiper.js'
+
+
+import 'bootstrap'; // поменять путь
+// import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
