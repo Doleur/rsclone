1485de3ffe81b3c7be9d37e519871f5206374fc1
Alena-Zykava <zykava.alena@gmail.com>
2021-01-13 21:05:58 +0300

    feat: slide

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+import 'bootstrap';
 import './assets/scss/style.scss'
 import './assets/scss/swiper.scss'
 import './assets/scss/custom.scss'
 import './assets/scss/tab.scss'
 
-import './assets/js/test.js'
 import './assets/js/swiper.js'
+import './assets/js/test.js'
 import './assets/js/healthbar.js';
 
-import 'bootstrap'; // поменять путь
+ // поменять путь
 // import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
