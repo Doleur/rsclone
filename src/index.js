95191fea65a76ed758c95a3ab0bea7cb6cc47154
Doleur <voleur16@gmail.com>
2021-01-17 19:42:53 +0300

    feat: add drop golds

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import './assets/scss/style.scss';
 import './assets/scss/swiper.scss';
 import './assets/scss/custom.scss';
 import './assets/scss/tab.scss';
+import './assets/scss/gold.scss';
 
 import './assets/js/swiper.js';
 import './assets/js/test.js';
 import './assets/js/healthbar.js';
+// import './assets/js/gold.js';
 
-import './assets/js/monster.js';
 
 // поменять путь
 // import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
