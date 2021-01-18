4216d5d7a8065a0ab79d84cb752429c24a585ff2
JenkJS <jenkjobs@gmail.com>
2021-01-18 14:51:55 +0300

    feat: add backround-2

---
+++ src/assets/js/monster.js
@@ -0,0 +1,19 @@
+const monsters = [
+  'Angry_Potato',
+  'Catra',
+  'Catra-defeat',
+  'Finky',
+  'Flowers_Bloop',
+  'Gerbeel',
+  'Gerbeelpillar',
+  'Gloop',
+  'Impossumble',
+  'Loggernaut',
+  'Sasquish',
+  'Mushroom_Bloop',
+  'Spit_Flower',
+  'Treasure_Chest-hit',
+  'Tyrantula'
+]
+
+export { monsters }
