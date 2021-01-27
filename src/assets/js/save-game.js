075fd04db3e8ca034bca8b0400a9f2dbe5396af7
Zykava Alena <zykava.alena@gmail.com>
2021-01-27 15:07:25 +0300

    fead: localStorage shop

--- src/assets/js/save-game.js
+++ src/assets/js/save-game.js
@@ -1,5 +1,6 @@
 import { gameStats } from './constants.js'
 import { arrLevel } from './healthbar.js'
+import { heroesData } from './heroesData.js'
 
 function setCount() {
   const saveItems = {
@@ -7,12 +8,19 @@ function setCount() {
     autoDPS: gameStats.DPS.number,
     damage: gameStats.clickDamage.number,
     currLevel: gameStats.currLevel,
-    health: gameStats.health,
-    currMonster: gameStats.currMonster,
+    health: gameStats.health.number,
     arrLevel
   }
   const serialSaveItems = JSON.stringify(saveItems)
   localStorage.setItem('saveItems', serialSaveItems)
 }
 
-export { setCount }
+function setLevelHeros () {
+  const saveLvl = heroesData.map((item) => {
+    return item.lvl
+  })
+  const serialSaveLvl = JSON.stringify(saveLvl)
+  localStorage.setItem('saveLvl', serialSaveLvl)
+}
+
+export { setCount, setLevelHeros }
